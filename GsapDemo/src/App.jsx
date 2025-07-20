7f44d6ae8560c4e64007a8370e7ad3f023ede480
rohit8866seervi <rohitseervi8866@gmail.com>
2025-07-20 23:50:57 +0530

    created random spining square

--- GsapDemo/src/App.jsx
+++ GsapDemo/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap';
+import React, { useRef, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+
+const App = () => {
+  // gsap.registerPlugin(useGSAP);
+  const X=gsap.utils.random(-window.screen.availWidth/2,window.screen.availWidth/2,100);
+  const Y=gsap.utils.random(-window.screen.availHeight/2,window.screen.availHeight/2,100);
+  const [xValue,setxValue]=useState(0);
+  const [yValue,setyValue]=useState(0);
+  const [rotateValue,setrotateValue]=useState(0);
+  console.log(window.screen)
+  const boxRef=useRef();
+  useGSAP(()=>{
+    console.log(xValue)
+    console.log(yValue)
+    gsap.to(boxRef.current,{
+      x:xValue,
+      y:yValue,
+      rotation:rotateValue,
+      duration:2
+    })
+  },[rotateValue])
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-black w-full h-screen flex flex-col items-center justify-center gap-7 overflow-hidden'>
+      <div 
+      ref={boxRef} 
+      className='h-30 w-30 bg-gradient-to-br from-blue-800 to-blue-400 rounded-3xl'>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button 
+      className='bg-gradient-to-br from-emerald-600 to-emerald-800 text-red-50 p-5 pt-2 pb-2 rounded-2xl z-10' 
+      onClick={()=>{setxValue(X);setyValue(Y);setrotateValue(prev=>prev+360)}}>
+        change
+      </button>
+    </div>
   )
 }
 
